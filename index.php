ccbd26ddb47a79d15cacdc61f1b8f39f2adadb25
zachary egle <35083488+zewebdesign@users.noreply.github.com>
2019-01-29 16:47:42 -0700

    Add files via upload
    update: final version

---
+++ index.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php include("includes/header.php");?> <!-- meta data, links, title, ect -->
+    </head>
+    <body>
+        <header class="header">
+            <?php include("includes/menu.php");?> <!-- navigation setup -->
+            <div class="header-text-box col12xs">
+                <h1 class="heading-primary mb-1">
+                    <span class="heading-primary-main">“Lorem ipsum dolor sit amet, con</span>
+                </h1>
+                <h1 class="heading-primary">
+                    <span class="heading-primary-sub">adipiscing eliAt. Curabitur nec lacus La purus"</span>
+                </h1>
+            </div>
+        </header>
+
+        <main>
+            <section class="section-services">
+                <div class="wrapper">
+                    <div class="mb-4">
+                        <h2 class="heading-secondary">Services</h2>
+                    </div>
+
+                    <div class="container">
+                        <div class="row">
+                            <div class="col3l col3m col3s">
+                                <img src="images/archive.png" alt="storage" class="icon mb-1">
+                                <h3 class="heading-tertiary mb-1">Web Design</h3>
+                                <p class="paragraph mb-6">Apple pie icing sweet. Brownie jelly-o applicake applicake sweet roll liquorice bear claw. Jujubes carrot cake cotton
+                                    candy sweet tart brownie. Tiramisu applicake jujubes.</p>
+                            </div>
+                            <div class="col3l col3m col3s">
+                                <img src="images/camera.png" alt="camera" class="icon mb-1">
+                                <h3 class="heading-tertiary mb-1">Photography</h3>
+                                <p class="paragraph mb-6">Apple pie icing sweet. Brownie jelly-o applicake applicake sweet roll liquorice bear claw. Jujubes carrot cake cotton
+                                    candy sweet tart brownie. Tiramisu applicake jujubes.</p>
+                            </div>
+                            <div class="col3l col3m col3s">
+                                <img src="images/dslr.png" alt="digital camera" class="icon mb-1">
+                                <h3 class="heading-tertiary mb-1">Graphics Design</h3>
+                                <p class="paragraph mb-6">Apple pie icing sweet. Brownie jelly-o applicake applicake sweet roll liquorice bear claw. Jujubes carrot cake cotton
+                                    candy sweet tart brownie. Tiramisu applicake jujubes.</p>
+                            </div>
+                            <div class="col3l col3m col3s">
+                                <img src="images/frame.png" alt="frame" class="icon mb-1">
+                                <h3 class="heading-tertiary mb-1">Branding</h3>
+                                <p class="paragraph mb-6">Apple pie icing sweet. Brownie jelly-o applicake applicake sweet roll liquorice bear claw. Jujubes carrot cake cotton
+                                    candy sweet tart brownie. Tiramisu applicake jujubes.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="section-portfolio">
+                <div class="wrapper">
+                    <?php include("includes/portfolio.php");?>
+                </div>
+            </section>
+
+            <section class="section-awesome">
+                <div class="wrapper">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col5l col5m col12s mb-6 mb-autoxs mb-autos">
+                                <div class="mb-4 mb-2m">
+                                    <h2 class="heading-secondary">Sweet Awesomeness</h2>
+                                </div>
+                                <div class="awesome-text">
+                                    <p class="paragraph mb-2">
+                                        Bear claw marzipan bear claw applicake I love muffin. Lemon drops gummi bears pastry gummi bears sesame snaps I love unerdwear.com.
+                                        Soufflé cotton candy dessert candy ice cream wafer gummies cheesecake brownie.
+                                    </p>
+                                    <p class="paragraph">
+                                        Muffin chupa chups jelly beans sweet pie applicake. Croissant chocolate cake I love pudding. Ice cream I love powder pudding
+                                        apple pie marshmallow. Cupcake marzipan oat cake bonbon I love candy canes toffee.
+                                    </p>
+                                </div>
+                                <a href="#" class="btn-text mt-4 mt-2m">Visit Me</a>
+                            </div>
+                            <div class="col1l"></div>
+                            <div class="col6l col7m col12s col12xs mb-6">
+                                <img src="images/video.jpg" alt="vimeo video" class="awesome-video">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="section-social">
+                <div class="wrapper">
+                    <div class="mb-4">
+                        <ul class="heading-list">
+                            <li>
+                                <h2 class="heading-quad-white">Twitter</h2>
+                            </li>
+                            <li>
+                                <i class="fab fa-twitter white"></i>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="container">
+                        <div class="row">
+                            <div class="col12l col12m social mb-6 mb-8m mb-8s">
+                                <p class="social-text">
+                                    Unerdwear cookie liquorice. Cake donut cupcake lollipop soufflé candy. Chocolate oat cake
+                                    <a href="#" class="social-link">@cheesecake</a> tootsie roll.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="section-about">
+                <div class="wrapper">
+                    <div class="mb-4">
+                        <h2 class="heading-secondary">Who Am I?</h2>
+                    </div>
+
+                    <div class="container">
+                        <div class="row">
+                            <div class="col6l col6m">
+                                <div class="about-text">
+                                    <p class="paragraph mb-2">
+                                        Bear claw marzipan bear claw applicake I love muffin. Lemon drops gummi bears pastry gummi bears sesame snaps I love unerdwear.com.
+                                        Soufflé cotton candy dessert candy ice cream wafer gummies cheesecake brownie.
+                                    </p>
+                                    <p class="paragraph">
+                                        Muffin chupa chups jelly beans sweet pie applicake. Croissant chocolate cake I love pudding. Ice cream I love powder pudding
+                                        apple pie marshmallow. Cupcake marzipan oat cake bonbon I love candy canes toffee.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="col6l col6m mb-14 mb-8s">
+                                <div class="about-text">
+                                    <p class="paragraph mb-2">
+                                        Pudding dessert jujubes tiramisu gingerbread croissant tiramisu applicake. Sesame snaps sugar plum cotton candy chocolate
+                                        bonbon lemon drops candy canes cotton candy. Cake toffee pie bear claw pastry.
+                                    </p>
+                                    <ul class="about-icons">
+                                        <li>
+                                            <a href="#" class="about-icon1">
+                                                <i class="fab fa-twitter"></i>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#" class="about-icon2">
+                                                <i class="fab fa-google-plus-g"></i>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#" class="about-icon3">
+                                                <i class="fas fa-coffee"></i>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="section-feedback">
+                <div class="wrapper">
+                    <div class="mb-4">
+                        <ul class="heading-list">
+                            <li>
+                                <h2 class="heading-quad">Testimonials</h2>
+                            </li>
+                            <li>
+                                <i class="fas fa-quote-right black"></i>
+                            </li>
+                        </ul>
+                    </div>
+            
+                    <div class="container">
+                        <div class="row">
+                            <div class="col12l feedback mb-6">
+                                <p class="feedback-text mb-4">
+                                    Applicake chocolate cake wafer toffee pie soufflé wafer. Tart marshmallow wafer macaroon cheesecake jelly. Gingerbread cookie
+                                    soufflé sweet roll sweet roll jelly-o.
+                                </p>
+                                <p class="paragraph">-Brooks the Bison</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="section-contact">
+                <div class="wrapper">
+                    <div class="mb-4">
+                        <h2 class="heading-secondary">Contact</h2>
+                    </div>
+                    <?php include("includes/contact-form.php");?>
+                </div>
+            </section>
+
+
+        </main>
+        <?php include("includes/footer.php");?>
+
+        <!-- page modals -->
+        <div id="img-modal" class="modal">
+            <img src="" alt="modal image" id="modal-images" class="modal-content">
+        </div>
+    
+        <script type="text/javascript" src="js/portfolio.js"></script>
+    </body>
+</html>
